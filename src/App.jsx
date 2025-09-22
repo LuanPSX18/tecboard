c5180011b77d3d8955a6b5755e16d30687caa1e0
LuanPSX18 <luanplaystation@gmail.com>
2025-09-22 18:33:20 -0300

    primerio commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css'
+
+// no react, componentes são FUNÇÕES
+
+function FormularioDeEvento () {
+  return (
+    <form className='form-evento'>
+      <h2>Preencha para criar um evento:</h2>
+      <fieldset>
+        <label htmlFor="nome">
+          Qual nome do evento?
+        </label>
+        <input type="text" id='nome'/>
+      </fieldset>
+    </form>
+  )
+}
+
+function App() {
+  return (
+    <main>
+      <header>
+        <img src="/logo.png" alt="" />
+      </header>
+      <section>
+        <img src="banner.png" alt="" />
+      </section>
+      <FormularioDeEvento />
+    </main> 
+  )
+}
+
+export default App
